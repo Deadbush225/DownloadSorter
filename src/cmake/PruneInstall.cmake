5a6b0c79a3350838de9143bfd8d6e71a0d5e0fa9
Eliazar Inso <75558018+Deadbush225@users.noreply.github.com>
2025-08-24 23:45:16 +0800

    chore(build): remove deprecated folders

--- src/cmake/PruneInstall.cmake
+++ src/cmake/PruneInstall.cmake
@@ -1,34 +1,34 @@
 # Runs at install time to remove optional/banned files from the install tree
 
 # remove software OpenGL
-file(REMOVE "${CMAKE_INSTALL_PREFIX}/bin/opengl32sw.dll")
+file(REMOVE "${CMAKE_INSTALL_PREFIX}/opengl32sw.dll")
 
 # remove QtNetwork just in case
 file(REMOVE
-    "${CMAKE_INSTALL_PREFIX}/bin/Qt6Network.dll"
-    "${CMAKE_INSTALL_PREFIX}/bin/Qt6Networkd.dll")
+    "${CMAKE_INSTALL_PREFIX}/Qt6Network.dll"
+    "${CMAKE_INSTALL_PREFIX}/Qt6Networkd.dll")
 
 # remove plugin folders we don't want
 file(REMOVE_RECURSE
-    "${CMAKE_INSTALL_PREFIX}/bin/tls"
-    "${CMAKE_INSTALL_PREFIX}/bin/networkinformation"
-    "${CMAKE_INSTALL_PREFIX}/bin/bearer")
+    "${CMAKE_INSTALL_PREFIX}/tls"
+    "${CMAKE_INSTALL_PREFIX}/networkinformation"
+    "${CMAKE_INSTALL_PREFIX}/bearer")
 
 # remove qgif and qjpeg plugins explicitly
-file(GLOB _qgif "${CMAKE_INSTALL_PREFIX}/bin/imageformats/qgif*.dll")
+file(GLOB _qgif "${CMAKE_INSTALL_PREFIX}/imageformats/qgif*.dll")
 
 if(_qgif)
     file(REMOVE ${_qgif})
 endif()
 
-file(GLOB _qjpeg "${CMAKE_INSTALL_PREFIX}/bin/imageformats/qjpeg*.dll")
+file(GLOB _qjpeg "${CMAKE_INSTALL_PREFIX}/imageformats/qjpeg*.dll")
 
 if(_qjpeg)
     file(REMOVE ${_qjpeg})
 endif()
 
 # remove TUIO touch plugin explicitly
-file(GLOB _qtuiot "${CMAKE_INSTALL_PREFIX}/bin/generic/qtuiotouchplugin*.dll")
+file(GLOB _qtuiot "${CMAKE_INSTALL_PREFIX}/generic/qtuiotouchplugin*.dll")
 
 if(_qtuiot)
     file(REMOVE ${_qtuiot})
@@ -36,8 +36,8 @@ endif()
 
 # remove OpenSSL if it slipped in
 file(GLOB _openssl
-    "${CMAKE_INSTALL_PREFIX}/bin/libssl*.dll"
-    "${CMAKE_INSTALL_PREFIX}/bin/libcrypto*.dll")
+    "${CMAKE_INSTALL_PREFIX}/libssl*.dll"
+    "${CMAKE_INSTALL_PREFIX}/libcrypto*.dll")
 
 if(_openssl)
     file(REMOVE ${_openssl})
@@ -45,8 +45,8 @@ endif()
 
 # remove D3DCompiler (ANGLE) if copied
 file(GLOB _d3dc
-    "${CMAKE_INSTALL_PREFIX}/bin/d3dcompiler*.dll"
-    "${CMAKE_INSTALL_PREFIX}/bin/D3DCompiler*.dll")
+    "${CMAKE_INSTALL_PREFIX}/d3dcompiler*.dll"
+    "${CMAKE_INSTALL_PREFIX}/D3DCompiler*.dll")
 
 if(_d3dc)
     file(REMOVE ${_d3dc})
@@ -54,6 +54,6 @@ endif()
 
 # remove MinGW runtime DLLs if present (for static runtime builds)
 file(REMOVE
-    "${CMAKE_INSTALL_PREFIX}/bin/libgcc_s_seh-1.dll"
-    "${CMAKE_INSTALL_PREFIX}/bin/libstdc++-6.dll"
-    "${CMAKE_INSTALL_PREFIX}/bin/libwinpthread-1.dll")
+    "${CMAKE_INSTALL_PREFIX}/libgcc_s_seh-1.dll"
+    "${CMAKE_INSTALL_PREFIX}/libstdc++-6.dll"
+    "${CMAKE_INSTALL_PREFIX}/libwinpthread-1.dll")
