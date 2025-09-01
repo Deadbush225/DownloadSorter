90751b16211c5e8f15558fa69122bc34a47faa8b
Deadbush225 <eliazar225ninso@gmail.com>
2025-09-01 10:42:01 -0400

    attempt to decrease windows installer size

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 git add -A
-git commit -m "updated build-release.yml for windows"
+git commit -m "attempt to decrease windows installer size"
 git tag -f v1.5.2
 git push origin main --tags -f
--- src/cmake/PruneInstall.cmake
+++ src/cmake/PruneInstall.cmake
@@ -1,59 +1,68 @@
 # Runs at install time to remove optional/banned files from the install tree
 
-# remove software OpenGL
-file(REMOVE "${CMAKE_INSTALL_PREFIX}/opengl32sw.dll")
+# Common helper to safely remove matching files
+function(_rm_glob)
+    file(GLOB _tmp ${ARGV})
 
-# remove QtNetwork just in case
-file(REMOVE
-    "${CMAKE_INSTALL_PREFIX}/Qt6Network.dll"
-    "${CMAKE_INSTALL_PREFIX}/Qt6Networkd.dll")
+    if(_tmp)
+        file(REMOVE ${_tmp})
+    endif()
+endfunction()
 
-# remove plugin folders we don't want
+# Remove software OpenGL
+_rm_glob("${CMAKE_INSTALL_PREFIX}/opengl32sw.dll")
+_rm_glob("${CMAKE_INSTALL_PREFIX}/Qt6OpenGL*.dll")
+_rm_glob("${CMAKE_INSTALL_PREFIX}/Qt6OpenGLWidgets*.dll")
+_rm_glob("${CMAKE_INSTALL_PREFIX}/Qt6ANGLE*.dll")
+
+# Remove debug builds
+_rm_glob("${CMAKE_INSTALL_PREFIX}/*d.dll")
+
+# Remove QtNetwork (if not required at runtime)
+_rm_glob("${CMAKE_INSTALL_PREFIX}/Qt6Network.dll")
+
+# Remove ICU and DBus if present (not needed for basic widgets apps on MSVC)
+_rm_glob("${CMAKE_INSTALL_PREFIX}/icudt*.dll")
+_rm_glob("${CMAKE_INSTALL_PREFIX}/icuin*.dll")
+_rm_glob("${CMAKE_INSTALL_PREFIX}/icuuc*.dll")
+_rm_glob("${CMAKE_INSTALL_PREFIX}/Qt6DBus*.dll")
+
+# Remove plugin folders we don't want
 file(REMOVE_RECURSE
     "${CMAKE_INSTALL_PREFIX}/tls"
     "${CMAKE_INSTALL_PREFIX}/networkinformation"
-    "${CMAKE_INSTALL_PREFIX}/bearer")
+    "${CMAKE_INSTALL_PREFIX}/bearer"
+)
 
-# remove qgif and qjpeg plugins explicitly
-file(GLOB _qgif "${CMAKE_INSTALL_PREFIX}/imageformats/qgif*.dll")
+# Remove image format plugins we don’t use (keep png)
+file(GLOB _img_unneeded
+    "${CMAKE_INSTALL_PREFIX}/imageformats/qgif*.dll"
+    "${CMAKE_INSTALL_PREFIX}/imageformats/qjpeg*.dll"
+    "${CMAKE_INSTALL_PREFIX}/imageformats/qsvg*.dll"
+)
 
-if(_qgif)
-    file(REMOVE ${_qgif})
+if(_img_unneeded)
+    file(REMOVE ${_img_unneeded})
 endif()
 
-file(GLOB _qjpeg "${CMAKE_INSTALL_PREFIX}/imageformats/qjpeg*.dll")
+# Remove TUIO touch plugin
+_rm_glob("${CMAKE_INSTALL_PREFIX}/generic/qtuiotouchplugin*.dll")
 
-if(_qjpeg)
-    file(REMOVE ${_qjpeg})
-endif()
-
-# remove TUIO touch plugin explicitly
-file(GLOB _qtuiot "${CMAKE_INSTALL_PREFIX}/generic/qtuiotouchplugin*.dll")
-
-if(_qtuiot)
-    file(REMOVE ${_qtuiot})
-endif()
-
-# remove OpenSSL if it slipped in
-file(GLOB _openssl
+# Remove OpenSSL if it slipped in
+_rm_glob(
     "${CMAKE_INSTALL_PREFIX}/libssl*.dll"
-    "${CMAKE_INSTALL_PREFIX}/libcrypto*.dll")
+    "${CMAKE_INSTALL_PREFIX}/libcrypto*.dll"
+)
 
-if(_openssl)
-    file(REMOVE ${_openssl})
-endif()
-
-# remove D3DCompiler (ANGLE) if copied
-file(GLOB _d3dc
+# Remove D3DCompiler (ANGLE) if copied
+_rm_glob(
     "${CMAKE_INSTALL_PREFIX}/d3dcompiler*.dll"
-    "${CMAKE_INSTALL_PREFIX}/D3DCompiler*.dll")
+    "${CMAKE_INSTALL_PREFIX}/D3DCompiler*.dll"
+)
 
-if(_d3dc)
-    file(REMOVE ${_d3dc})
-endif()
-
-# remove MinGW runtime DLLs if present (for static runtime builds)
-file(REMOVE
+# Remove MinGW runtime DLLs (not applicable on MSVC, but safe if present)
+_rm_glob(
     "${CMAKE_INSTALL_PREFIX}/libgcc_s_seh-1.dll"
     "${CMAKE_INSTALL_PREFIX}/libstdc++-6.dll"
-    "${CMAKE_INSTALL_PREFIX}/libwinpthread-1.dll")
+    "${CMAKE_INSTALL_PREFIX}/libwinpthread-1.dll"
+)
